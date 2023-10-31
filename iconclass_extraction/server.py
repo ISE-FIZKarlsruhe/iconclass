42a64f715a8f6ea6a63a4b43d7a54001f94bebda
westoun <christoph.werner.stein@gmail.com>
2023-10-31 17:37:28 +0100

    Implement iconclass code extraction

--- iconclass_extraction/server.py
+++ iconclass_extraction/server.py
@@ -4,9 +4,12 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, Response, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
+import spacy
 from typing import List
 
 load_dotenv()
+
+nlp = spacy.load("./data/output/model-last")
 
 app = FastAPI()
 
@@ -14,7 +17,32 @@ templates = Jinja2Templates(directory="templates")
 
 
 def extract_iconclass_codes(text: str) -> List[str]:
-    pass
+    doc = nlp(text)
+
+    codes = []
+    continued_codes = []
+
+    for ent in doc.ents:
+        if ent.label_ == "IC_START":
+            codes.append(ent.text.strip())
+
+        elif ent.label_ == "IC_CONTD":
+            # edge case, where wrongly detected cont before
+            # first starting iconclass code.
+            if len(codes) == 0:
+                continue
+
+            cont_code = ent.text.replace("+", "").replace("(", "").replace(")", "")
+
+            if " " in codes[-1]:
+                new_code = f"{codes[-1]} {cont_code}"
+            else:
+                new_code = f"{codes[-1]}{cont_code}"
+
+            continued_codes.append(new_code)
+
+    codes = codes + continued_codes
+    return codes
 
 
 @app.get("/")
@@ -31,7 +59,7 @@ async def get_iconclass_codes(request: Request, response: Response):
         return response
 
     annotations = []
-    for text in body["text"]:
+    for text in body["texts"]:
         codes = extract_iconclass_codes(text)
         annotations.append({"text": text, "codes": codes})
 
